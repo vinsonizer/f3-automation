22d77ef7b9cc5f8914f8a1d563eda87fc132121b
Jason Vinson <vinsonizer@gmail.com>
2016-05-21 21:15:08 -0400

    Added lots of comments
    More robust commenting to make things clearer for others

--- src/backblasts.js
+++ src/backblasts.js
@@ -1,28 +1,45 @@
+/*
+ * A Google Apps Script to subscribe to a Regional F3 RSS feed.  It will
+ * parse the feed and look for new entries.  For each new entry, it will
+ * pull the HTML for the backblast and parse out relevant info for tracking.
+ *
+ * @author Gears (Jason Vinson)
+ *
+ * config is a global config object.  Anyone who adopts this script should
+ * only have to change these three values to get this working for their region.
+ *
+ * url: Feed by region, if you go to your schedules landing page you find it
+ *
+ * fileId: File Id for Google sheet.  Open Sheet in browser and copy
+ * string after the d/ up to /edit
+ *
+ * sheetName: Name of the sheet inside the google sheet, assumes Columns are:
+ *  Date, Categories, Count, Url
+ */
 var config = {
-    // Feed by region, if you go to your schedules landing page you find it
     url: "http://f3nation.com/locations/fort-mill-sc/feed/",
-    /*
-    File Id for Google sheet.  Open Sheet in browser and copy
-    string after the d/ up to /edit
-    */
     fileId: "1B5l_olGDsHI8fL_kzR9h4V5lFrJysB3a6xvU7sct7lk",
-    /*
-    Name of the sheet inside the google sheet
-    Assumes Columns are:
-    Date, Categories, Count, Url
-    */
     sheetName: "BB Counts"
 };
 
-/*
-Most of this code was contributed by Wingman (Trent Jones)
-*/
-function myFunction() {
+
+/**
+ * Main function that should be invoked by trigger
+ */
+function checkBackblasts() {
     var url = config.url;
     main(url);
 }
 
-/* Credit: https://gist.github.com/agektmr */
+
+/**
+ * Main fuction to drive the logic for the script.
+ * Most of this code was originally contributed by Wingman (Trent Jones)
+ *
+ * @param {String} url The RSS URL to monitor
+ *
+ * Credit: https://gist.github.com/agektmr
+ */
 function main(url) {
 
     var ss = getSheet();
@@ -45,12 +62,23 @@ function main(url) {
     property.setProperty('last_update', date.getTime());
 }
 
+/**
+ * Get the configured sheet object to write to.
+ * @return {Object} configured sheet reference.
+ */
 function getSheet() {
     var file = SpreadsheetApp.openById(config.fileId);
     var sheet = file.getSheetByName(config.sheetName);
     return sheet;
 }
 
+/**
+ * Takes text of an RSS feed, parses to XML objects and then returns
+ * and array of items for processing
+ *
+ * @param {String} feed A textual representation of an RSS feed.
+ * @return {Object} Array of items for processing.
+ */
 function getItems(feed) {
     var doc = XmlService.parse(feed);
     var root = doc.getRootElement();
@@ -58,6 +86,14 @@ function getItems(feed) {
     var items = channel.getChildren('item');
     return items;
 }
+
+/**
+ * Takes an item and a sheet reference.  Inserts the item values in row two,
+ * basically dynamically growing the sheet in a Stack (LIFO) apprach.
+ *
+ * @param {Object} item an XML object representing a feed item
+ * @param {Object} sheet a reference to the configured Google sheet
+ */
 
 function insertRow(item, sheet) {
     var title = item.getChildText('title');
@@ -75,6 +111,14 @@ function insertRow(item, sheet) {
     ]);
 }
 
+/**
+ * This function takes an RSS feed item, connects to the URL of the feed
+ * entry.  It then uses regex to pull the date and the pax count from the HTML
+ * as well as the category from the feed object.
+ *
+ * @param {Object} item XML object representing the feed item
+ * @return {Object} an object with the date, pax count, pax list, and Categories
+ */
 function getAdditionalData(item) {
     var url = item.getChildText('link');
     var body = UrlFetchApp.fetch(url).getContentText();
