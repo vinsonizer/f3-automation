c311cd8c0f0a38742229cf84f81643a3e75b9fcb
Jason Vinson <vinsonizer@gmail.com>
2016-05-25 19:16:12 -0400

    removed default file id

--- cfg/config.js
+++ cfg/config.js
@@ -17,7 +17,7 @@ function getConfiguration() {
      */
     var backblast_config = {
         url: "http://f3nation.com/locations/fort-mill-sc/feed/",
-        fileId: "1B5l_olGDsHI8fL_kzR9h4V5lFrJysB3a6xvU7sct7lk",
+        fileId: "your-file-id-here",
         sheetName: "BB Counts"
     };
 
