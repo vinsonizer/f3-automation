3860d738207f81a947d12d7d5908f90db77b00aa
Jason Vinson <vinsonizer@gmail.com>
2016-06-14 08:16:31 -0400

    fixed indentation

--- src/backblasts.js
+++ src/backblasts.js
@@ -12,8 +12,8 @@
  * Main function that should be invoked by trigger
  */
 function checkBackblasts() {
-    var config = getConfiguration();
-    main(config.backblast_config);
+  var config = getConfiguration();
+  main(config.backblast_config);
 }
 
 
@@ -27,25 +27,25 @@ function checkBackblasts() {
  */
 function main(cfg) {
 
-    var url = cfg.url;
-    var ss = getSheet(cfg);
+  var url = cfg.url;
+  var ss = getSheet(cfg);
 
-    var property = PropertiesService.getScriptProperties();
-    var last_update = property.getProperty('last_update');
-    last_update = last_update === null ? 0 : parseFloat(last_update);
+  var property = PropertiesService.getScriptProperties();
+  var last_update = property.getProperty('last_update');
+  last_update = last_update === null ? 0 : parseFloat(last_update);
 
-    var feed = UrlFetchApp.fetch(url).getContentText();
-    var items = getItems(feed);
-    var i = items.length - 1;
-    var date = new Date();
-    while (i > -1) {
-        var item = items[i--];
-        date = new Date(item.getChildText('pubDate'));
-        if (date.getTime() > last_update) {
-            insertRow(item, ss);
-        }
+  var feed = UrlFetchApp.fetch(url).getContentText();
+  var items = getItems(feed);
+  var i = items.length - 1;
+  var date = new Date();
+  while (i > -1) {
+    var item = items[i--];
+    date = new Date(item.getChildText('pubDate'));
+    if (date.getTime() > last_update) {
+      insertRow(item, ss);
     }
-    property.setProperty('last_update', date.getTime());
+  }
+  property.setProperty('last_update', date.getTime());
 }
 
 /**
@@ -55,9 +55,9 @@ function main(cfg) {
  * @return {Object} configured sheet reference.
  */
 function getSheet(cfg) {
-    var file = SpreadsheetApp.openById(cfg.fileId);
-    var sheet = file.getSheetByName(cfg.countsSheetName);
-    return sheet;
+  var file = SpreadsheetApp.openById(cfg.fileId);
+  var sheet = file.getSheetByName(cfg.countsSheetName);
+  return sheet;
 }
 
 /**
@@ -68,11 +68,11 @@ function getSheet(cfg) {
  * @return {Object} Array of items for processing.
  */
 function getItems(feed) {
-    var doc = XmlService.parse(feed);
-    var root = doc.getRootElement();
-    var channel = root.getChild('channel');
-    var items = channel.getChildren('item');
-    return items;
+  var doc = XmlService.parse(feed);
+  var root = doc.getRootElement();
+  var channel = root.getChild('channel');
+  var items = channel.getChildren('item');
+  return items;
 }
 
 /**
@@ -84,19 +84,19 @@ function getItems(feed) {
  */
 
 function insertRow(item, sheet) {
-    var title = item.getChildText('title');
-    var url = item.getChildText('link');
-    var author = item.getChildText('author');
+  var title = item.getChildText('title');
+  var url = item.getChildText('link');
+  var author = item.getChildText('author');
 
 
-    var additional = getAdditionalData(item);
-    var date = new Date(item.getChildText('pubDate'));
-    sheet.insertRowBefore(2);
-    sheet.getRange('A2:D2').setValues([
-        [
-            additional.date, additional.category, additional.paxCount, url
-        ]
-    ]);
+  var additional = getAdditionalData(item);
+  var date = new Date(item.getChildText('pubDate'));
+  sheet.insertRowBefore(2);
+  sheet.getRange('A2:D2').setValues([
+    [
+      additional.date, additional.category, additional.paxCount, url
+    ]
+  ]);
 }
 
 /**
@@ -108,50 +108,57 @@ function insertRow(item, sheet) {
  * @return {Object} an object with the date, pax count, pax list, and Categories
  */
 function getAdditionalData(item) {
-    var url = item.getChildText('link');
-    var body = UrlFetchApp.fetch(url).getContentText();
+  var url = item.getChildText('link');
+  var body = UrlFetchApp.fetch(url).getContentText();
 
-    var qicRegex = /QIC:<\/strong>([^<]*)<\/li>/;
-    var paxRegex = /The PAX:<\/strong>([^<]*)<\/li>/;
-    var whenRegex = /When:<\/strong>([^<]*)<\/li>/;
-    var paxList = "";
-    var paxCount = 0;
-    var when = "";
-    var qic = "";
-    var paxMatch = paxRegex.exec(body);
-    var qicMatch = qicRegex.exec(body);
-    var whenMatch = whenRegex.exec(body) && whenRegex.exec(body).length > 0 ? whenRegex.exec(body)[1].trim() : '';
-    if (paxMatch) {
-        // in case we ever want to capture the actual pax list
-        paxList = paxMatch[1].split(",").map(clean);
-        paxCount = paxList.length;
+  var qicRegex = /QIC:<\/strong>([^<]*)<\/li>/;
+  var paxRegex = /The PAX:<\/strong>([^<]*)<\/li>/;
+  var whenRegex = /When:<\/strong>([^<]*)<\/li>/;
+  var paxList = "";
+  var paxCount = 0;
+  var when = "";
+  var qic = "";
+  var paxMatch = paxRegex.exec(body);
+  var qicMatch = qicRegex.exec(body);
+  var whenMatch = whenRegex.exec(body) && whenRegex.exec(body).length > 0 ? whenRegex.exec(body)[1].trim() : '';
+  if (paxMatch) {
+    // in case we ever want to capture the actual pax list
+    paxList = paxMatch[1].split(",").map(clean);
+    paxCount = paxList.length;
+  }
+  if (qicMatch) {
+    qic = qicMatch[1];
+    if (paxList && paxList.indexOf(qic) == -1) {
+      paxCount++;
+      paxList.push(qic);
     }
-    if (qicMatch) {
-        qic = qicMatch[1];
-        if(paxList && paxList.indexOf(qic) == -1) {
-            paxCount++;
-            paxList.push(qic);
-        }
-    }
-    if (whenMatch) {
-        when = whenMatch;
-    }
+  }
+  if (whenMatch) {
+    when = whenMatch;
+  }
 
-    var cats = item.getChildren('category');
+  var cats = item.getChildren('category');
 
-    var c = "";
-    for (var i = 0; i < cats.length; i++) {
-        c = cats[i].getText();
-    }
+  var c = "";
+  for (var i = 0; i < cats.length; i++) {
+    c = cats[i].getText();
+  }
 
-    return {
-        date: when,
-        paxCount: paxCount,
-        paxList: paxList,
-        category: c
-    };
+  return {
+    date: when,
+    paxCount: paxCount,
+    paxList: paxList,
+    category: c
+  };
 }
 
 function clean(input) {
-    return input.replace(/[^A-Za-z0-9]/g,"").toLowerCase().trim();
+  return input.replace(/[^A-Za-z0-9]/g, "").toLowerCase().trim();
+}
+
+if (typeof exports !== 'undefined') {
+  exports.clean = clean;
+  exports.getAdditionalData = getAdditionalData;
+  exports.getItems = getItems;
+  exports.main = main;
 }
