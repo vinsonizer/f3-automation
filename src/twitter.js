58e4318b29932630facda0faa809f80013835272
Jason Vinson <vinsonizer@gmail.com>
2016-06-20 17:02:13 -0400

    made code more OOP

--- src/twitter.js
+++ src/twitter.js
@@ -1,83 +1,70 @@
 //https://script.google.com/macros/d/1nA0fj4a8Fga-whToZEzeqOW0gwDvpSmhav66ZT1qUa37LvX6bLoSZTgW/usercallback
 
 function TwitterClient(service, cfg) {
+  this.cfg = cfg;
   this.propertyKey = cfg.consumerKey + '_latest_tweet_date';
+  this.service = service;
+}
 
-  this.getLatestTweetDate = function() {
-    var latestTweetDateStr = PropertiesService.getScriptProperties().getProperty(this.propertyKey) || '2016/01/01';
-    var latestTweetDate = new Date(latestTweetDateStr);
-    return latestTweetDate;
-  };
-
-  this.setLatestTweetDate = function(latestTweetDate) {
-    // always check for the day prior, no way to include time component
+TwitterClient.prototype = {
+  constructor: TwitterClient,
+  searchForTweets: function() {
+    // check since yesterday, no way to pass specific time
+    var latestTweetDate = new Date();
     latestTweetDate.setDate(latestTweetDate.getDate() - 1);
-    PropertiesService.getScriptProperties().setProperty(this.propertyKey, latestTweetDate.toString());
-    Logger.log("Latest Retweet Date set to " + latestTweetDate.toString());
-  };
-
-
-  this.searchForTweets = function() {
-    var latestTweetDate = this.getLatestTweetDate();
     var sinceDate = latestTweetDate.getFullYear() + "-" + (latestTweetDate.getMonth() + 1) + "-" + latestTweetDate.getDate();
-    var url = 'https://api.twitter.com/1.1/search/tweets.json?q=' + encodeURIComponent(cfg.retweetMonitoringSearch + " since:" + sinceDate);
-    var response = service.fetch(url, {
+    var url = 'https://api.twitter.com/1.1/search/tweets.json?q=' + encodeURIComponent(this.cfg.retweetMonitoringSearch + " since:" + sinceDate);
+    var response = this.service.fetch(url, {
       method: 'get'
     });
     var tweets = JSON.parse(response.getContentText()).statuses;
     Logger.log("Found " + tweets.length + " for query with since date of " + sinceDate);
     return tweets;
-  };
-
-  this.retweet = function(tweet) {
+  },
+  retweet: function(tweet) {
     var rt_url = 'https://api.twitter.com/1.1/statuses/retweet/' + tweet.id_str + '.json';
-    var rt_response = service.fetch(rt_url, {
+    var rt_response = this.service.fetch(rt_url, {
       method: 'post'
     });
     var rt_result = JSON.parse(rt_response.getContentText());
-  };
-
-  this.processRetweets = function() {
-    var latestTweetDate = this.getLatestTweetDate();
-    if (service.hasAccess()) {
+  },
+  processRetweets: function() {
+    if (this.service.hasAccess()) {
       var tweets = this.searchForTweets();
       for (var i = tweets.length - 1; i >= 0; i--) {
         try {
           var tweet = tweets[i];
           Logger.log("Found tweet from " + tweet.created_at + " with text: " + tweet.text);
-          var tweetDate = new Date(tweet.created_at);
-          if (tweetDate > latestTweetDate) {
-            latestTweetDate = tweetDate;
-            this.retweet(tweet);
-          }
+          this.retweet(tweet);
         } catch (error) {
-          Logger.log(cfg.consumerKey + " => " + error);
+          Logger.log(error);
         }
       }
-      this.setLatestTweetDate(latestTweetDate);
     } else {
-      var authorizationUrl = service.authorize();
+      var authorizationUrl = this.service.authorize();
       Logger.log('Open the following URL and re-run the script: %s',
         authorizationUrl);
     }
-  };
-}
+  }
+};
+
+function TwitterService() {}
 
 // encapsulation for testing
-function TwitterService() {
-
+TwitterService.prototype = {
+  constructor: TwitterService,
   /**
    * Reset the authorization state, so that it can be re-tested.
    */
-  this.reset = function() {
+  reset: function() {
     var service = getService();
     service.reset();
-  };
+  },
 
   /**
    * Configures the service.
    */
-  this.getService = function() {
+  getService: function() {
     var cfg = getConfiguration().twitter_config;
     return OAuth1.createService('Twitter')
       // Set the endpoint URLs.
@@ -92,12 +79,12 @@ function TwitterService() {
       .setCallbackFunction('authCallback')
       // Set the property store where authorized tokens should be persisted.
       .setPropertyStore(PropertiesService.getUserProperties());
-  };
+  },
 
   /**
    * Handles the OAuth callback.
    */
-  this.authCallback = function(request) {
+  authCallback: function(request) {
     var service = getService();
     var authorized = service.handleCallback(request);
     if (authorized) {
@@ -105,8 +92,8 @@ function TwitterService() {
     } else {
       return HtmlService.createHtmlOutput('Denied');
     }
-  };
-}
+  }
+};
 
 function authCallback(request) {
   new TwitterService().authCallback(request);
