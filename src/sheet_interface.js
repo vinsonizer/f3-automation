2f2374b264c264d196a9389c3362143c2afcd2f2
Jason Vinson <vinsonizer@gmail.com>
2016-09-22 19:22:42 -0400

    changed to DocumentProperties

--- src/sheet_interface.js
+++ src/sheet_interface.js
@@ -2,11 +2,11 @@ var SHEET_ID = "com.f3nation.automation.SHEET_ID";
 
 function _init() {
   var sheetId = SpreadsheetApp.getActiveSpreadsheet().getId();
-  PropertiesService.getScriptProperties().setProperty(SHEET_ID, sheetId);
+  PropertiesService.getDocumentProperties().setProperty(SHEET_ID, sheetId);
 }
 
 function getSpreadsheet() {
-  return SpreadsheetApp.openById(PropertiesService.getScriptProperties().getProperty(SHEET_ID));
+  return SpreadsheetApp.openById(PropertiesService.getDocumentProperties().getProperty(SHEET_ID));
 }
 
 function onOpen() {
