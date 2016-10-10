1f7c9f03db268ca44da1d4369356f31dc6df3ec8
Jason Vinson <vinsonizer@gmail.com>
2016-10-10 14:56:08 -0400

    ported test cases, only need to update sheet iface

--- test/backblasts_tests.js
+++ test/backblasts_tests.js
@@ -6,34 +6,33 @@ var bb = require("../src/backblasts.js").backblasts;
 services = require("../src/services.js").services;
 
 var WHEN = "<li><strong>When:</strong>01/01/2016</li>";
-var QIC = "<li><strong>QIC:</strong>Wingman</li>";
-var PAX = "<li><strong>The PAX:</strong> The Once-ler, Waterfoot, Vida, Chin Music, Crayola, Bullwinkle (FNG), Hannibal, Knight Rider, MAD, Pele, Adobe, Smash, Balk, Fireman Ed, Marge, Lambeau, Torpedo, Goonie (QIC) </li>";
+var QIC = "<li><strong>QIC:</strong>Zima, Bolt, Bing</li>";
+var PAX = "<li><strong>The PAX:</strong>Wingman,  Gears, Old Bay, Bing</li>";
 
 var UPDATE_DATE = new Date().toString();
 
 describe('Backblasts Data', function() {
+  var fetch_stub = sinon.stub(services, 'fetch');
+  var parse_xml_stub = sinon.stub(services, 'parse_xml');
 
   var mockDoc = function() {
     var docMock = {
       getRootElement: function() {},
       getChild: function() {},
-      getChildren: function() {}
-    };
-
-    var itemMock = {
       getChildText: function() {},
       getChildren: function() {}
     };
 
     sinon.stub(docMock, 'getRootElement').returns(docMock);
     sinon.stub(docMock, 'getChild').withArgs('channel').returns(docMock);
-    sinon.stub(docMock, 'getChildren').withArgs('item').returns([itemMock]);
 
-    var childText = sinon.stub(itemMock, 'getChildText');
+    var getChildren = sinon.stub(docMock, 'getChildren');
+    getChildren.withArgs('item').returns([docMock]);
+    getChildren.withArgs('category').returns(['category1', 'category2']);
+
+    var childText = sinon.stub(docMock, 'getChildText');
     childText.withArgs('link').returns("http://testurl.com");
     childText.withArgs('pubDate').returns(UPDATE_DATE);
-
-    sinon.stub(itemMock, 'getChildren').withArgs('category').returns(['category1', 'category2']);
 
     return docMock;
   };
@@ -43,93 +42,126 @@ describe('Backblasts Data', function() {
   describe('checkForUpdates', function() {
     it('should parse out workout dates', function() {
 
-      var fetch = sinon.stub(services, 'fetch');
-      fetch.returns(WHEN + QIC + PAX);
+      fetch_stub.returns(WHEN + QIC + PAX);
 
       var docMock = mockDoc();
-      sinon.stub(services, 'parse_xml').returns(docMock);
+      parse_xml_stub.returns(docMock);
 
       var result = bb.checkForUpdates(
         cfg,
-        // last update date
-        function() {
-          var yesterday = new Date().getDate() - 1;
-          return yesterday.toString();
-        },
-        // latest update date
-        function(newDate) {
-          // check date here
-          assert(newDate === UPDATE_DATE, 'Should record the new date');
-        },
-        // row values
+        function() {},
+        function(newDate) {},
         function(rowValues) {
-          // check Row Values here
           assert(rowValues[0] === "01/01/2016", "Should parse out date");
-          assert(rowValues[1] === "category1,category2", "Should parse out categories");
-          assert(rowValues[2] === 19, "Should parse out pax count");
-          assert(rowValues[3] === "http://testurl.com", "Should parse out bb url");
         });
     });
-    /*
-                it('should parse out todays date if workout date not found', function() {
-                  var content = "<li><strong></li>";
-                  var result = bb.getAdditionalData(content, []);
-                  var date = new Date(result.date);
-                  var today = new Date();
+    it('should parse out todays date if workout date not found', function() {
 
-                  assert(date.toLocaleDateString() == today.toLocaleDateString(), "Should be equal to today");
-                }); it('should parse out pax count and list', function() {
-                  var content = "<li><strong>The PAX:</strong>Wingman</li>";
-                  var result = bb.getAdditionalData(content, []);
-                  assert(result.paxCount == 1, "Should parse out pax list");
-                  assert(result.paxList.indexOf("wingman") != -1, "Should parse out pax list");
-                });
+      fetch_stub.returns(QIC + PAX);
 
-                it('should parse out pax count', function() {
-                  var content = "The PAX:</strong> The Once-ler, Waterfoot, Vida, Chin Music, Crayola, Bullwinkle (FNG), Hannibal, Knight Rider, MAD, Pele, Adobe, Smash, Balk, Fireman Ed, Marge, Lambeau, Torpedo, Goonie (QIC) </li>";
-                  var result = bb.getAdditionalData(content, []);
-                  assert(result.paxList.length === 18, "Should find the number of Pax");
-                }); it('should parse out QIC', function() {
-                  var content = "<li><strong>QIC:</strong>Gears</li>";
-                  var result = bb.getAdditionalData(content, []);
-                  assert(result.paxCount == 1, "Should parse out pax list");
-                  assert(result.paxList.indexOf("gears") != -1, "Should parse out pax list");
-                }); it('should handle QIC in pax list and QIC field', function() {
-                  var content = "<li><strong>QIC:</strong>Gears</li>" +
-                    "<li><strong>The PAX:</strong>Wingman,  Gears</li>";
-                  var result = bb.getAdditionalData(content, []);
-                  assert(result.paxCount == 2, "Should parse out pax list");
-                  assert(result.paxList.indexOf("gears") != -1, "Should capture QIC as a PAX");
-                  assert(result.paxList.indexOf("wingman") != -1, "Should capture non-QIC as a PAX");
-                }); it('should handle multiple in QIC field with and', function() {
-                  var content = "<li><strong>QIC:</strong>Gears and Old Bay</li>" +
-                    "<li><strong>The PAX:</strong>Wingman,  Gears, Old Bay</li>";
-                  var result = bb.getAdditionalData(content, []);
-                  assert(result.paxCount == 3, "Should parse out pax list");
-                  assert(result.paxList.indexOf("gears") != -1, "Should capture QIC as a PAX");
-                  assert(result.paxList.indexOf("oldbay") != -1, "Should capture QIC as a PAX");
-                  assert(result.paxList.indexOf("wingman") != -1, "Should capture non-QIC as a PAX");
-                }); it('should handle multiple in QIC field with commas', function() {
-                  var content = "<li><strong>QIC:</strong>Zima, Bolt, Bing</li>" +
-                    "<li><strong>The PAX:</strong>Wingman,  Gears, Old Bay</li>";
-                  var result = bb.getAdditionalData(content, []);
-                  assert(result.paxCount == 6, "Should parse out pax list");
-                  assert(result.paxList.indexOf("zima") != -1, "Should capture QIC as a PAX");
-                  assert(result.paxList.indexOf("bolt") != -1, "Should capture QIC as a PAX");
-                  assert(result.paxList.indexOf("bing") != -1, "Should capture QIC as a PAX");
-                  assert(result.paxList.indexOf("wingman") != -1, "Should capture non-QIC as a PAX");
-                  assert(result.paxList.indexOf("gears") != -1, "Should capture non-QIC as a PAX");
-                  assert(result.paxList.indexOf("oldbay") != -1, "Should capture non-QIC as a PAX");
-                }); it('should handle handle common acronyms', function() {
-                  var content = "<li><strong>QIC:</strong>Gears(YHC)</li>" +
-                    "<li><strong>The PAX:</strong>Wingman (Respect), Bolt(QIC), Sharkbait (2.0)</li>";
-                  var result = bb.getAdditionalData(content, []);
-                  assert(result.paxCount == 4, "Should parse out pax list");
-                  assert(result.paxList.indexOf("bolt") != -1, "Should contain bolt");
-                  assert(result.paxList.indexOf("wingman") != -1, "Should contain wingman");
-                  assert(result.paxList.indexOf("gears") != -1, "Should contain gears");
-                  assert(result.paxList.indexOf("sharkbait") != -1, "Should contain sharkbait");
-                });
-                */
+      var docMock = mockDoc();
+      parse_xml_stub.returns(docMock);
+      var result = bb.checkForUpdates(
+        cfg,
+        function() {},
+        function(newDate) {},
+        function(rowValues) {
+          var dt = new Date();
+          assert(rowValues[0] === (dt.getMonth() + 1) + "/" + dt.getDate() + "/" + dt.getYear(), "Should set missing date to today");
+        });
+
+    });
+
+    it('should parse out pax count and list', function() {
+      fetch_stub.returns(PAX);
+
+      var docMock = mockDoc();
+      parse_xml_stub.returns(docMock);
+      var result = bb.checkForUpdates(
+        cfg,
+        function() {},
+        function(newDate) {},
+        function(rowValues) {
+          assert(rowValues[2] === 4, "Should parse out pax count");
+        });
+    });
+    it('should parse out pax count', function() {
+      var content = "The PAX:</strong> The Once-ler, Waterfoot, Vida, Chin Music, Crayola, Bullwinkle (FNG), Hannibal, Knight Rider, MAD, Pele, Adobe, Smash, Balk, Fireman Ed, Marge, Lambeau, Torpedo, Goonie (QIC) </li>";
+      fetch_stub.returns(content);
+      var result = bb.checkForUpdates(
+        cfg,
+        function() {},
+        function(newDate) {},
+        function(rowValues) {
+          assert(rowValues[2] === 18, "Should parse out pax count");
+        });
+    });
+    it('should parse out QIC', function() {
+      var content = "<li><strong>QIC:</strong>Gears</li>";
+      fetch_stub.returns(content);
+      var result = bb.checkForUpdates(
+        cfg,
+        function() {},
+        function(newDate) {},
+        function(rowValues) {
+          assert(rowValues[2] === 1, "Should parse out pax count");
+        });
+    });
+    it('should handle QIC in pax list and QIC field', function() {
+      var content = "<li><strong>QIC:</strong>Gears</li>" +
+        "<li><strong>The PAX:</strong>Wingman,  Gears</li>";
+      fetch_stub.returns(content);
+      var result = bb.checkForUpdates(
+        cfg,
+        function() {},
+        function(newDate) {},
+        function(rowValues) {
+          assert(rowValues[2] === 2, "Should parse out pax count and remove dupes from QIC");
+        });
+
+    });
+    it('should handle multiple in QIC field with and', function() {
+      var content = "<li><strong>QIC:</strong>Gears and Old Bay</li>" +
+        "<li><strong>The PAX:</strong>Wingman,  Gears, Old Bay</li>";
+      fetch_stub.returns(content);
+      var result = bb.checkForUpdates(
+        cfg,
+        function() {},
+        function(newDate) {},
+        function(rowValues) {
+          assert(rowValues[2] === 3, "Should handle multiple QIC and parse out 'and'");
+        });
+    });
+    it('should handle multiple in QIC field with commas', function() {
+      var content = "<li><strong>QIC:</strong>Zima, Bolt, Bing</li>" +
+        "<li><strong>The PAX:</strong>Wingman,  Gears, Old Bay</li>";
+      fetch_stub.returns(content);
+      var result = bb.checkForUpdates(
+        cfg,
+        function() {},
+        function(newDate) {},
+        function(rowValues) {
+          assert(rowValues[2] === 6, "Should handle multiple QIC and parse out 'commas'");
+        });
+    });
+    it('should handle handle common acronyms', function() {
+      var content = "<li><strong>QIC:</strong>Gears(YHC)</li>" +
+        "<li><strong>The PAX:</strong>Wingman (Respect), Bolt(QIC), Sharkbait (2.0)</li>";
+      fetch_stub.returns(content);
+      var result = bb.checkForUpdates(
+        cfg,
+        function() {},
+        function(newDate) {},
+        function(rowValues) {
+          assert(rowValues[2] === 4, "Should handle multiple QIC and parse out 'commas'");
+        },
+        function(rowValues) {
+          var paxList = rowValues[2];
+          assert(paxList.indexOf("bolt") != -1, "Should contain bolt");
+          assert(paxList.indexOf("wingman") != -1, "Should contain wingman");
+          assert(paxList.indexOf("gears") != -1, "Should contain gears");
+          assert(paxList.indexOf("sharkbait") != -1, "Should contain sharkbait");
+        });
+    });
   });
 });
