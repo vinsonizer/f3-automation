14b8656e931d31e1240b279ddb6009acafe2ca00
Jason Vinson <vinsonizer@gmail.com>
2016-10-11 14:57:29 -0400

    beginning of large twitter refactor

--- src/twitter.js
+++ src/twitter.js
@@ -1,186 +1,100 @@
-//https://script.google.com/macros/d/1nA0fj4a8Fga-whToZEzeqOW0gwDvpSmhav66ZT1qUa37LvX6bLoSZTgW/usercallback
+var TWITTER_BASE_URL = 'https://api.twitter.com/1.1/';
 
-function twitterAuthCallback(request) {
-    new TwitterService().twitterAuthCallback(request);
-}
+var twitter = {};
 
-function twitterResetAuth() {
-    new TwitterService().reset();
-}
-
-function twitterProcessRetweets() {
-    var client = new TwitterClient(new TwitterService().getService(), getConfig());
-    client.processRetweets();
-}
-
-function twitterCountsPolling() {
-    var client = new TwitterClient(new TwitterService().getService(), getConfig());
-    client.pullCounts();
-}
-
-function TwitterClient(service, cfg) {
-    this.cfg = cfg.twitter_config;
-    this.service = service;
-}
-
-TwitterClient.prototype = {
-    constructor: TwitterClient,
-
-    searchForTweets: function(searchString) {
-        // check since yesterday, no way to pass specific time
-        var latestTweetDate = new Date();
-        latestTweetDate.setDate(latestTweetDate.getDate() - 1);
-        var sinceDate = latestTweetDate.getFullYear() + "-" + (latestTweetDate.getMonth() + 1) + "-" + latestTweetDate.getDate();
-        var url = 'https://api.twitter.com/1.1/search/tweets.json?q=' + encodeURIComponent(searchString + " since:" + sinceDate);
-        var response = this.service.fetch(url, {
-            method: 'get'
-        });
-        var tweets = JSON.parse(response.getContentText()).statuses;
-        Logger.log("Found " + tweets.length + " for query with since date of " + sinceDate);
-        return tweets;
-    },
-
-    retweet: function(tweet, binding) {
-        var rt_url = 'https://api.twitter.com/1.1/statuses/retweet/' + tweet.id_str + '.json';
-        var rt_response = binding.service.fetch(rt_url, {
-            method: 'post'
-        });
-        var rt_result = JSON.parse(rt_response.getContentText());
-    },
-
-    getTweet: function(idString) {
-      var tweet_url = 'https://api.twitter.com/1.1/statuses/show/' + idString + '.json';
-      var tweet_response = this.service.fetch(tweet_url, {
-          method: 'get'
-      });
-      var resultTweet = JSON.parse(tweet_response.getContentText());
-      return resultTweet;
-    },
-
-    handleTweets: function(tweets, binding, callback) {
-        for (var i = tweets.length - 1; i >= 0; i--) {
-            try {
-                var tweet = tweets[i];
-                Logger.log("Found tweet from " + tweet.created_at + " with text: " + tweet.text);
-                callback(tweet, binding);
-            } catch (error) {
-                Logger.log(error);
-            }
-        }
-    },
-
-    processRetweets: function() {
-      if(this.cfg.retweetMonitoringSearch) {
-        if (this.service.hasAccess()) {
-            var tweets = this.searchForTweets(this.cfg.retweetMonitoringSearch);
-            this.handleTweets(tweets, this, this.retweet);
-        } else {
-          _showAuthDialog(this.service.authorize());
-        }
-      }
-    },
-
-    pullCounts: function() {
-      if(this.cfg.countsMonitoringSearch) {
-        // TODO: Dry this
-        if (this.service.hasAccess()) {
-            var tweets = this.searchForTweets(this.cfg.countsMonitoringSearch);
-            this.handleTweets(tweets, this, this.logTweet);
-        } else {
-          _showAuthDialog(this.service.authorize());
-        }
-      }
-    },
-
-    logTweet: function(tweet, binding) {
-        var self = binding;
-        if(tweet.is_quote_status) {
-          tweet = self.getTweet(tweet.quoted_status_id_str);
-        }
-        var getMatches = function(inputText, regex, position) {
-            var matches = [];
-            var match;
-
-            while ((match = regex.exec(inputText))) {
-                matches.push(match[position]);
-            }
-            return matches;
-        };
-        var tags = getMatches(tweet.text, /(?:^|\s)(?:#)([a-zA-Z\d]+)/gm, 1);
-        var numbers = getMatches(tweet.text, /\b([0-9]+)\b/g, 0);
-        var tweetId = tweet.id_str;
-
-        var file = SpreadsheetApp.getActiveSpreadsheet();
-        var sheet = file.getSheetByName(self.cfg.tweetCountSheetName);
-
-        var rangeValues = sheet.getRange("A2:A52");
-        // check last 50
-        var idRange = Array.apply(null, Array(50)).map(function(_, i) {
-            return rangeValues.getCell(i + 1, 1).getValue();
-        });
-
-        if (idRange.indexOf(tweetId) === -1) {
-            sheet.insertRowBefore(2);
-            sheet.getRange('A2:F2').setValues([
-                [
-                    tweetId, tweet.user.screen_name, tweet.created_at, numbers.toString(), tags.toString(), tweet.text
-                ]
-            ]);
-        }
-    }
+twitter.get_service = function(cfg) {
+  return services.get_oauth_service({
+    service_name: 'twitter',
+    access_token_url: 'https://api.twitter.com/oauth/access_token',
+    request_token_url: 'https://api.twitter.com/oauth/request_token',
+    authorization_url: 'https://api.twitter.com/oauth/authorize',
+    consumer_key: cfg.consumerKey,
+    consumer_secret: cfg.consumerSecret,
+    callback_function: 'twitterAuthCallback'
+  });
 };
 
-function TwitterService() {}
-
-// encapsulation for testing
-TwitterService.prototype = {
-    constructor: TwitterService,
-    /**
-     * Reset the authorization state, so that it can be re-tested.
-     */
-    reset: function() {
-        var service = this.getService();
-        service.reset();
-    },
-
-    /**
-     * Configures the service.
-     */
-    getService: function() {
-        var cfg = getConfig().twitter_config;
-        return OAuth1.createService('Twitter')
-            // Set the endpoint URLs.
-            .setAccessTokenUrl('https://api.twitter.com/oauth/access_token')
-            .setRequestTokenUrl('https://api.twitter.com/oauth/request_token')
-            .setAuthorizationUrl('https://api.twitter.com/oauth/authorize')
-            // Set the consumer key and secret.
-            .setConsumerKey(cfg.consumerKey)
-            .setConsumerSecret(cfg.consumerSecret)
-            // Set the name of the callback function in the script referenced
-            // above that should be invoked to complete the OAuth flow.
-            .setCallbackFunction('twitterAuthCallback')
-            // Set the property store where authorized tokens should be persisted.
-            .setPropertyStore(PropertiesService.getUserProperties());
-    },
-
-    /**
-     * Handles the OAuth callback.
-     */
-    twitterAuthCallback: function(request) {
-        var service = this.getService();
-        var authorized = service.handleCallback(request);
-        if (authorized) {
-            return HtmlService.createHtmlOutput('Success!');
-        } else {
-            return HtmlService.createHtmlOutput('Denied');
-        }
-    }
+twitter.search_for_tweets = function(searchString, opts) {
+  // check since yesterday, no way to pass specific time
+  var latestTweetDate = new Date();
+  latestTweetDate.setDate(latestTweetDate.getDate() - 1);
+  var sinceDate = latestTweetDate.getFullYear() + "-" + (latestTweetDate.getMonth() + 1) + "-" + latestTweetDate.getDate();
+  var url = TWITTER_BASE_URL + '/search/tweets.json?q=' + encodeURIComponent(searchString + " since:" + sinceDate);
+  opts.method = 'get';
+  var response = services.fetch(url, twitter.get_service(cfg), opts);
+  var tweets = services.parse_json(response).statuses;
+  services.log("Found " + tweets.length + " for query with since date of " + sinceDate + " with opts " + opts);
+  return tweets;
 };
 
+twitter.retweet = function(tweet) {
+  var rt_url = TWITTER_BASE_URL + '/statuses/retweet/' + tweet.id_str + '.json';
+  var rt_response = services.fetch(rt_url, twitter.get_service, {
+    method: 'post'
+  });
+  return services.parse_json(rt_response);
+};
 
+twitter.get_tweet = function(id_string) {
+  var tweet_url = 'https://api.twitter.com/1.1/statuses/show/' + id_string + '.json';
+  var tweet_response = services.fetch(tweet_url, twitter.get_service, {
+    method: 'get'
+  });
+  var resultTweet = services.parse_json(tweet_response);
+  return resultTweet;
+};
+
+twitter.log_tweet = function(tweet, log_tweet_callback) {
+  if (tweet.is_quote_status) {
+    tweet = twitter.get_tweet(tweet.quoted_status_id_str);
+  }
+  var getMatches = function(inputText, regex, position) {
+    var matches = [];
+    var match;
+
+    while ((match = regex.exec(inputText))) {
+      matches.push(match[position]);
+    }
+    return matches;
+  };
+  var tags = getMatches(tweet.text, /(?:^|\s)(?:#)([a-zA-Z\d]+)/gm, 1);
+  var numbers = getMatches(tweet.text, /\b([0-9]+)\b/g, 0);
+  var tweetId = tweet.id_str;
+  log_tweet_callback([
+    tweetId, tweet.user.screen_name, tweet.created_at, numbers.toString(), tags.toString(), tweet.text
+  ]);
+};
+
+twitter.tweet_handler = function(search_string, callback, additional_args) {
+  var handleTweets = function(tweets, callback, additional_args) {
+    for (var i = tweets.length - 1; i >= 0; i--) {
+      try {
+        var tweet = tweets[i];
+        Logger.log("Found tweet from " + tweet.created_at + " with text: " + tweet.text);
+        callback(tweet, additional_args);
+      } catch (error) {
+        Logger.log(error);
+      }
+    }
+  };
+
+  if (twitter.get_service().hasAccess()) {
+    var tweets = twitter.search_for_tweets(search_string);
+    handleTweets(tweets, callback, additional_args);
+  } else {
+    services.show_auth_dialog(twitter.get_service().authorize());
+  }
+};
+
+twitter.retweet_search_tweets = function(cfg) {
+  twitter.tweet_handler(cfg.retweetMonitoringSearch, twitter.retweet);
+};
+
+twitter.log_search_tweets = function(cfg, sheet_log_callback) {
+  twitter.tweet_handler(cfg.countsMonitoringSearch, twitter.log_tweet, sheet_log_callback);
+};
 
 // this block is for when running in node outside of GAS
 if (typeof exports !== 'undefined') {
-    exports.TwitterService = TwitterService;
-    exports.TwitterClient = TwitterClient;
+  exports.twitter = twitter;
 }
