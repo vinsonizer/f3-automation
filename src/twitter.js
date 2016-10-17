0bcfbf3330305b9f2f401957fe5b1fef015afb08
Jason Vinson <vinsonizer@gmail.com>
2016-10-16 20:24:19 -0400

    Beginnings of Twitter test harness

--- src/twitter.js
+++ src/twitter.js
@@ -14,39 +14,41 @@ twitter.get_service = function(cfg) {
   });
 };
 
-twitter.search_for_tweets = function(searchString, opts) {
+twitter.search_for_tweets = function(oauth_service, searchString) {
   // check since yesterday, no way to pass specific time
   var latestTweetDate = new Date();
   latestTweetDate.setDate(latestTweetDate.getDate() - 1);
   var sinceDate = latestTweetDate.getFullYear() + "-" + (latestTweetDate.getMonth() + 1) + "-" + latestTweetDate.getDate();
   var url = TWITTER_BASE_URL + '/search/tweets.json?q=' + encodeURIComponent(searchString + " since:" + sinceDate);
-  opts.method = 'get';
-  var response = services.fetch(url, twitter.get_service(cfg), opts);
+  var opts = {
+    method: 'get'
+  };
+  var response = services.fetch(url, oauth_service, opts);
   var tweets = services.parse_json(response).statuses;
   services.log("Found " + tweets.length + " for query with since date of " + sinceDate + " with opts " + opts);
   return tweets;
 };
 
-twitter.retweet = function(tweet) {
+twitter.retweet = function(twitter_service, tweet, retweet_callback) {
   var rt_url = TWITTER_BASE_URL + '/statuses/retweet/' + tweet.id_str + '.json';
-  var rt_response = services.fetch(rt_url, twitter.get_service, {
+  var rt_response = services.fetch(rt_url, twitter_service, {
     method: 'post'
   });
-  return services.parse_json(rt_response);
+  retweet_callback(services.parse_json(rt_response));
 };
 
-twitter.get_tweet = function(id_string) {
+twitter.get_tweet = function(twitter_service, id_string) {
   var tweet_url = 'https://api.twitter.com/1.1/statuses/show/' + id_string + '.json';
-  var tweet_response = services.fetch(tweet_url, twitter.get_service, {
+  var tweet_response = services.fetch(tweet_url, twitter_service, {
     method: 'get'
   });
   var resultTweet = services.parse_json(tweet_response);
   return resultTweet;
 };
 
-twitter.log_tweet = function(tweet, log_tweet_callback) {
+twitter.log_tweet = function(twitter_service, tweet, log_tweet_callback) {
   if (tweet.is_quote_status) {
-    tweet = twitter.get_tweet(tweet.quoted_status_id_str);
+    tweet = twitter.get_tweet(twitter_service, tweet.quoted_status_id_str);
   }
   var getMatches = function(inputText, regex, position) {
     var matches = [];
@@ -60,38 +62,39 @@ twitter.log_tweet = function(tweet, log_tweet_callback) {
   var tags = getMatches(tweet.text, /(?:^|\s)(?:#)([a-zA-Z\d]+)/gm, 1);
   var numbers = getMatches(tweet.text, /\b([0-9]+)\b/g, 0);
   var tweetId = tweet.id_str;
+  services.log("Tags: " + tags + ", numbers: " + numbers + ", tweetId: " + tweetId);
   log_tweet_callback([
     tweetId, tweet.user.screen_name, tweet.created_at, numbers.toString(), tags.toString(), tweet.text
   ]);
 };
 
-twitter.tweet_handler = function(search_string, callback, additional_args) {
-  var handleTweets = function(tweets, callback, additional_args) {
+twitter.tweet_handler = function(search_string, oauth_service, handler, callback) {
+  var handleTweets = function(oauth_service, tweets, handler, callback) {
     for (var i = tweets.length - 1; i >= 0; i--) {
       try {
         var tweet = tweets[i];
-        Logger.log("Found tweet from " + tweet.created_at + " with text: " + tweet.text);
-        callback(tweet, additional_args);
+        services.log("Found tweet from " + tweet.created_at + " with text: " + tweet.text);
+        handler(oauth_service, tweet, callback);
       } catch (error) {
-        Logger.log(error);
+        services.log(error);
       }
     }
   };
 
-  if (twitter.get_service().hasAccess()) {
-    var tweets = twitter.search_for_tweets(search_string);
-    handleTweets(tweets, callback, additional_args);
+  if (oauth_service.hasAccess()) {
+    var tweets = twitter.search_for_tweets(oauth_service, search_string);
+    handleTweets(oauth_service, tweets, handler, callback);
   } else {
-    services.show_auth_dialog(twitter.get_service().authorize());
+    services.show_auth_dialog(oauth_service.authorize());
   }
 };
 
-twitter.retweet_search_tweets = function(cfg) {
-  twitter.tweet_handler(cfg.retweetMonitoringSearch, twitter.retweet);
+twitter.retweet_search_tweets = function(cfg, retweet_callback) {
+  twitter.tweet_handler(cfg.retweetMonitoringSearch, twitter.get_service(cfg), twitter.retweet, retweet_callback);
 };
 
 twitter.log_search_tweets = function(cfg, sheet_log_callback) {
-  twitter.tweet_handler(cfg.countsMonitoringSearch, twitter.log_tweet, sheet_log_callback);
+  twitter.tweet_handler(cfg.countsMonitoringSearch, twitter.get_service(cfg), twitter.log_tweet, sheet_log_callback);
 };
 
 // this block is for when running in node outside of GAS
