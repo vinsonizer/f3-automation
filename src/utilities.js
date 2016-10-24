f37e80379247c8b937ae4060f17642e82ede1a1f
Jason Vinson <vinsonizer@gmail.com>
2016-10-24 12:31:11 -0400

    Huge refactor to make files have more concise purpose

--- src/utilities.js
+++
@@ -1,8 +0,0 @@
-function _wrapHtml(body) {
-  return "<html><head>" +
-    "<link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css'></link>" +
-    "<script type='javascript' href='https://code.jquery.com/jquery-3.0.0.min.js'></script>" +
-    "<meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no'></meta>" +
-    "<title>Newsletter Content</title></head><body><div class='container'>" +
-    body + "</container></body></html>";
-}
